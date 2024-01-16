6957057f706609a50b0370ba297ca8c0733afd76
ssslakter <67190162+Ssslakter@users.noreply.github.com>
2024-01-16 15:37:56 +0700

    add fastai usage and U-net

---
+++ ai/Unet.py
@@ -0,0 +1,155 @@
+from functools import wraps
+from torch import nn
+import math
+from einops.layers.torch import Rearrange
+import torch, torch.nn.functional as F, fastcore.all as fc
+
+
+def unet_conv(ni, nf, ks=3, stride=1, act=nn.SiLU, norm=None, bias=True):
+    layers = nn.Sequential()
+    if norm: layers.append(norm(ni))
+    if act : layers.append(act())
+    layers.append(nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2, bias=bias))
+    return layers
+
+def lin(ni, nf, act=nn.SiLU, norm=None, bias=True):
+    layers = nn.Sequential()
+    if norm: layers.append(norm(ni))
+    if act : layers.append(act())
+    layers.append(nn.Linear(ni, nf, bias=bias))
+    return layers
+
+
+def upsample(nf): return nn.Sequential(nn.Upsample(scale_factor=2.), nn.Conv2d(nf, nf, 3, padding=1))
+
+
+def saved(m, blk):
+    m_ = m.forward
+
+    @wraps(m.forward)
+    def _f(*args, **kwargs):
+        res = m_(*args, **kwargs)
+        blk.saved.append(res)
+        return res
+
+    m.forward = _f
+    return m
+
+
+def timestep_embedding(tsteps, emb_dim, max_period= 10000):
+    exponent = -math.log(max_period) * torch.linspace(0, 1, emb_dim//2, device=tsteps.device)
+    emb = tsteps[:,None].float() * exponent.exp()[None,:]
+    emb = torch.cat([emb.sin(), emb.cos()], dim=-1)
+    return F.pad(emb, (0,1,0,0)) if emb_dim%2==1 else emb
+
+
+class EmbResBlock(nn.Module):
+    def __init__(self, n_emb, ni, nf=None, ks=3, act=nn.SiLU, norm=nn.BatchNorm2d):
+        super().__init__()
+        if nf is None: nf = ni
+        self.emb_proj = nn.Linear(n_emb, nf*2)
+        self.conv1 = unet_conv(ni, nf, ks, act=act, norm=norm) #, bias=not norm)
+        self.conv2 = unet_conv(nf, nf, ks, act=act, norm=norm)
+        self.idconv = fc.noop if ni==nf else nn.Conv2d(ni, nf, 1)
+
+    def forward(self, x, t):
+        inp = x
+        x = self.conv1(x)
+        emb = self.emb_proj(F.silu(t))[:, :, None, None]
+        scale,shift = torch.chunk(emb, 2, dim=1)
+        x = x*(1+scale) + shift
+        x = self.conv2(x)
+        return x + self.idconv(inp)
+
+    
+class DownBlock(nn.Module):
+    def __init__(self, n_emb, ni, nf, add_down=True, num_layers=1):
+        super().__init__()
+        self.resnets = nn.ModuleList([saved(EmbResBlock(n_emb, ni if i==0 else nf, nf), self)
+                                      for i in range(num_layers)])
+        self.down = saved(nn.Conv2d(nf, nf, 3, stride=2, padding=1), self) if add_down else nn.Identity()
+
+    def forward(self, x, t):
+        self.saved = []
+        for resnet in self.resnets: x = resnet(x, t)
+        x = self.down(x)
+        return x
+    
+
+class SelfAttentionBlock(nn.Module):
+    def __init__(self, dim, heads = 8):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.to_qkv = nn.Linear(dim, dim * 3, bias = False)
+        self.to_out = nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.Dropout(0.1)
+        )
+
+        self.rearrange_qkv = Rearrange('b c (h w) -> (h w) b c', h = heads)
+        self.rearrange_out = Rearrange('(h w) b (p c) -> b (p c) (h w)', h = heads, p = -1)
+
+    def forward(self, x):
+        qkv = self.to_qkv(x)
+        q, k, v = self.rearrange_qkv(qkv).chunk(3, dim = -1)
+        dots = torch.einsum('b i d, b j d -> b i j', q, k) * self.scale
+        attn = dots.softmax(dim=-1)
+        out = torch.einsum('b i j, b j d -> b i d', attn, v)
+        out = self.rearrange_out(out)
+        return self.to_out(out)
+    
+
+class UpBlock(nn.Module):
+    def __init__(self, n_emb, ni, prev_nf, nf, add_up=True, num_layers=2):
+        super().__init__()
+        self.resnets = nn.ModuleList(
+            [EmbResBlock(n_emb, (prev_nf if i==0 else nf)+(ni if (i==num_layers-1) else nf), nf)
+            for i in range(num_layers)])
+        self.up = upsample(nf) if add_up else nn.Identity()
+
+    def forward(self, x, t, ups):
+        for resnet in self.resnets: x = resnet(torch.cat([x, ups.pop()], dim=1), t)
+        return self.up(x)
+    
+    
+class EmbUNetModel(nn.Module):
+    def __init__( self, in_channels=3, out_channels=3, nfs=(224,448,672,896), num_layers=1):
+        super().__init__()
+        self.conv_in = nn.Conv2d(in_channels, nfs[0], kernel_size=3, padding=1)
+        self.n_temb = nf = nfs[0]
+        n_emb = nf*4
+        # TODO: remove act func from 1st MLP layer
+        self.emb_mlp = nn.Sequential(lin(self.n_temb, n_emb, norm=nn.BatchNorm1d),
+                                     lin(n_emb, n_emb))
+        self.downs = nn.ModuleList()
+        for i in range(len(nfs)):
+            ni = nf
+            nf = nfs[i]
+            self.downs.append(DownBlock(n_emb, ni, nf, add_down=i!=len(nfs)-1, num_layers=num_layers))
+        self.mid_block = EmbResBlock(n_emb, nfs[-1])
+
+        rev_nfs = list(reversed(nfs))
+        nf = rev_nfs[0]
+        self.ups = nn.ModuleList()
+        for i in range(len(nfs)):
+            prev_nf = nf
+            nf = rev_nfs[i]
+            ni = rev_nfs[min(i+1, len(nfs)-1)]
+            self.ups.append(UpBlock(n_emb, ni, prev_nf, nf, add_up=i!=len(nfs)-1, num_layers=num_layers+1))
+        self.conv_out = unet_conv(nfs[0], out_channels, act=nn.SiLU, norm=nn.BatchNorm2d, bias=False)
+
+    def forward(self, inp):
+        x,t = inp
+        temb = timestep_embedding(t, self.n_temb)
+        emb = self.emb_mlp(temb)
+        x = self.conv_in(x)
+        saved = [x]
+        for block in self.downs: x = block(x, emb)
+        saved += [p for o in self.downs for p in o.saved]
+        x = self.mid_block(x, emb)
+        for block in self.ups: x = block(x, emb, saved)
+        return self.conv_out(x)
+    
+    
