1295f86e07a3db6a035d291279c4bf21ebab0f58
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2024-08-19 14:54:06 +0700

    little fixes

--- random_pixels/mode_filter.cu
+++ random_pixels/mode_filter.cu
@@ -31,7 +31,7 @@ __global__ void mode_filter2d_k(int *input, int *output, int w, int h, int r, in
         {
             if (i >= 0 && i < w && j >= 0 && j < h)
             {
-                &count[thread_id*k + input[i * h + j]]++;
+                count[thread_id * k + input[i * h + j]]++;
             }
         }
     }
@@ -40,14 +40,14 @@ __global__ void mode_filter2d_k(int *input, int *output, int w, int h, int r, in
     int idx_max = 0;
     for (int i = 0; i < k; i++)
     {
-        auto m = count[thread_id*k + i];
+        auto m = count[thread_id * k + i];
         if (m > max)
         {
             idx_max = i;
             max = m;
         }
     }
-    output[y*w + x] = idx_max;
+    output[y * w + x] = idx_max;
 }
 
 torch::Tensor mode_filter2d(torch::Tensor input, int r)
@@ -56,7 +56,11 @@ torch::Tensor mode_filter2d(torch::Tensor input, int r)
 
     auto output = torch::empty_like(input);
     auto k = std::get<0>(torch::_unique(input)).size(0);
-    TORCH_CHECK(input.max().item<int>() == k - 1 && input.min().item<int>() == 0,
+    if (k == 1)
+    {
+        return input;
+    }
+    TORCH_CHECK(input.max().item<int>() <= k && input.min().item<int>() >= 0,
                 "input must be a tensor of integers from 0 to k-1");
     dim3 block(BLOCK_SIZE, BLOCK_SIZE);
     dim3 grid((input.size(0) + BLOCK_SIZE - 1) / BLOCK_SIZE, (input.size(1) + BLOCK_SIZE - 1) / BLOCK_SIZE);
