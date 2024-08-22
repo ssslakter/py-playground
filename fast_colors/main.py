f52cbea8cb177349d8cf08dc9635a25691a87290
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2024-08-22 22:55:53 +0700

    add custom files and styles

--- fast_colors/main.py
+++ fast_colors/main.py
@@ -1,7 +1,8 @@
-from PIL import Image
+from io import BytesIO
 from colors import compress_img, extract_colors, scatter_plotly
 from fasthtml.common import *
 from fh_plotly import plotly2fasthtml, plotly_headers
+from PIL import Image
 
 gridlink = Link(rel="stylesheet",
                 href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css", type="text/css")
@@ -9,46 +10,64 @@ gridlink = Link(rel="stylesheet",
 headers = [picolink,
            gridlink,
            *plotly_headers,
-           Script(open(Path(__file__).parent / "markdown.js").read())]
+           Link(rel="stylesheet", href="/static/styles.css")]
 
 styles = [
     Style(".htmx-indicator {opacity:0;}"),
     Style(".htmx-indicator .htmx-request {opacity:1;}"),
 ]
 
-app, rt = fast_app(hdrs=headers+styles)
+app, rt = fast_app(hdrs=headers + styles)
 
 
 def Loader(id="loader"):
-    return Div("Loading...", id=id, cls="htmx-indicator")
+    return H2("Loading...", id=id, cls="htmx-indicator", style="text-align:center;")
 
 
 @rt('/')
 def get():
-    return Div(P('Hello World!'),
-               Div(*(image(id.strip('.png')) for id in os.listdir('./images')), cls="row"),
+    return Titled('Picture colors visualizer!',
+               P("This website allows you to visualize the colors of an image."),
+               P("You can upload your own images by using the upload button below or use the default images."),
+               Grid(*(image(id.strip('.png')) for id in os.listdir('../images'))),
+               Form(Input(name="file", type="file", accept="image/*"),
+                    Button("Upload"),
+                    hx_post="/colorize",
+                    hx_target="#main",
+                    hx_indicator="#loader",
+                    
+                    ),
+               P("You should see a scatter plot of the colors below."),
                Loader(),
                Div(id="main"))
 
 
 def image(id):
-    if os.path.exists(f"images/{id}.png"):
-        return Div(Img(src=f"/images/{id}.png", width=200),
+    return Div(Img(src=f"/images/{id}.png", width=200, height=200),
+                   style="display: flex;",
                    id=f'img-{id}',
                    hx_get=f"/colorize/{id}",
                    hx_target="#main",
                    hx_indicator="#loader")
-    else: return P(f'Image {id} not found')
 
 
-@rt('/colorize/{id}')
-def get(id: str):
-    img = Image.open(f"images/{id}.png").convert("RGB")
+def colorize(img):
     img = compress_img(img)
     img = extract_colors(img)
     fig = scatter_plotly(img)
     fig.update_layout(width=800, height=800)
     return Div(plotly2fasthtml(fig))
+
+@rt('/colorize/{id}')
+def get(id: str):
+    img = Image.open(f"images/{id}.png").convert("RGB")
+    return colorize(img)
+
+@rt('/colorize')
+async def post(file: UploadFile):
+    img = await file.read()
+    img = Image.open(BytesIO(img)).convert("RGB")
+    return colorize(img)
 
 
 serve()
